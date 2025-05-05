9246bb2cd35678af9f6fcdcd53061421c7f26be5
Mahesh5726 <maheshr070204@gmail.com>
2025-05-05 10:58:45 +0530

    Implement CORS middleware in Hono server and integrate authentication routes

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,22 @@
 import { serve } from '@hono/node-server'
 import { Hono } from 'hono'
+import { authenticationsRoutes } from './routes/authentications'
+import { cors } from 'hono/cors';
+import { webClientURL } from './utils/environment';
+const allRoutes = new Hono();
 
-const allRoutes = new Hono()
+allRoutes.use(
+  cors({
+    origin: webClientURL,
+    allowMethods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowHeaders: ["Authorization", "Content-Type"],
+    exposeHeaders: ["Content-Length"],
+    credentials: true,
+    maxAge: 600,
+  })
+);
 
-allRoutes.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+allRoutes.route("/authentications", authenticationsRoutes)
 
 serve(allRoutes, (info) => {
   console.log(`Server is running on http://localhost:${info.port}`)
