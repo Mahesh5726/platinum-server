de7a59789e4b18a2d956d626a3a9c71b1770a5ba
Mahesh5726 <maheshr070204@gmail.com>
2025-05-05 10:49:58 +0530

    Integrate Prisma adapter with betterAuth for PostgreSQL database support

--- src/integrations/better-auth/index.ts
+++ src/integrations/better-auth/index.ts
@@ -1,9 +1,12 @@
 import { betterAuth } from "better-auth";
 import { serverURL } from "../../utils/environment";
+import { prismaAdapter } from "better-auth/adapters/prisma";
+import { prismaClient } from "../prisma";
 
 export const betterAuthClient = betterAuth({
  baseURL: serverURL,
  basePath: "/authentications",
+ database: prismaAdapter(prismaClient, { provider: "postgresql" }),
  user: {
   modelName: "User",
  },
